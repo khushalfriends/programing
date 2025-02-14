ae406da7f6ee6d515f54dcc86d2f0e234b703978
khushalfriends <khusalagarwalla@gmail.com>
2025-02-14 19:14:59 +0530

    Add files via upload

---
+++ acp1.py
@@ -0,0 +1,10 @@
+grandpabday="25 march"
+grandmabday="15 september"
+mombday="18 november"
+dadbday="21 july"
+mybday="2 may"
+print(grandpabday)
+print(grandmabday)
+print(mombday)
+print(dadbday)
+print(mybday)
